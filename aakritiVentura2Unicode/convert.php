ec735d6ec1dac933da8e986cf9acb573b741dd57
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-04-19 19:24:24 +0530

    ventura akriti converter in place with samples

---
+++ aakritiVentura2Unicode/convert.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'converter.php';
+
+$converter = new Converter();
+
+
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-JAN.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-FEB.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-MAR.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-APR.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-MAY.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-JUNE.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-JULY.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-AUG.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-SEP.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-OCT.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-NOV.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-DEC.TXT');
+
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-ERA.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/P367-68.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/SANCOV.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/SANCOV1.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/SANSCR.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-PRE1.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-PRE1A.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-PRE2.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-PRE2A.TXT');
+$converter->process('/home/sriranga/Desktop/converters/aakritiVentura2Unicode/src/S-PRE3.TXT');
+
+?>
