a382a5f2b41c3f1aa460e0d17eddedac98024bd4
shivasdst <shivably04sdst@gmail.com>
2018-08-01 12:47:57 +0530

    shreelipi kannada to unicode converter added

--- menu.php
+++ menu.php
@@ -3,5 +3,6 @@
                     <li><a href="index.php">Aps2Unicode</a></li>
                     <li><a href="shreelipi.php">Shreelipi2Unicode(PDF)</a></li>
                     <li><a href="prajatounicode.php">Praja2Unicode</a></li>
+                    <li><a href="shreelipikan.php">Shreelipi2Unicode(Kannada)</a></li>
                 </ul>
             </div>
